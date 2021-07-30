ab5c9a42694a0a08da0f64accf8e40bbccca0c40
Swamp <swapnilskumars@gmail.com>
2021-07-30 09:45:39 +0530

    Environment variables fixed

--- settings/app_secrets.py
+++ settings/app_secrets.py
@@ -1,21 +1,21 @@
 import os
 
 secret = {
-  "type": os.environ('type'),
-  "project_id": os.environ('project_id'),
-  "private_key_id": os.environ('private_key_id'),
-  "private_key": os.environ('private_key'),
-  "client_email": os.environ('client_email'),
-  "client_id": os.environ('client_id'),
-  "auth_uri": os.environ('auth_uri'),
-  "token_uri": os.environ('token_uri'),
-  "auth_provider_x509_cert_url": os.environ('as1'),
-  "client_x509_cert_url": os.environ('as2')
+  "type": os.environ.get('type'),
+  "project_id": os.environ.get('project_id'),
+  "private_key_id": os.environ.get('private_key_id'),
+  "private_key": os.environ.get('private_key'),
+  "client_email": os.environ.get('client_email'),
+  "client_id": os.environ.get('client_id'),
+  "auth_uri": os.environ.get('auth_uri'),
+  "token_uri": os.environ.get('token_uri'),
+  "auth_provider_x509_cert_url": os.environ.get('as1'),
+  "client_x509_cert_url": os.environ.get('as2')
 }
 
 
-token = os.environ("TELEGRAM_BOT_TOKEN")
-Group_Owner = os.environ('group_owner')
-Telegram_Groupid = os.environ('telegram_groupid')
-DISTRICT_CODE = os.environ('district_code')
+token = os.environ.get("TELEGRAM_BOT_TOKEN")
+Group_Owner = os.environ.get('group_owner')
+Telegram_Groupid = os.environ.get('telegram_groupid')
+DISTRICT_CODE = os.environ.get('district_code')
 
