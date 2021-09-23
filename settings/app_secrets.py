266551b94440842383cac119a0b5f3008bc91e61
Swapnil-Kumar <swapnilskumars@gmail.com>
2021-09-23 15:50:40 +0530

    Evn variables

--- settings/app_secrets.py
+++ settings/app_secrets.py
@@ -6,16 +6,28 @@ import os
 
 # fireauths for under 45 group
 secret_under_45 = {
+  "type": "service_account",
   "project_id": os.environ.get('UNDER45_PROJECT_ID'),
   "private_key": os.environ.get('PRIVATE_KEY_UNDER45').replace('\\n', '\n'),
-  "client_email": os.environ.get('CLIENT_EMAIL_UNDER45')
+  "client_email": os.environ.get('CLIENT_EMAIL_UNDER45'),
+  "client_id": os.environ.get('U1'),
+  "auth_uri": os.environ.get('U2'),
+  "token_uri": os.environ.get('U3'),
+  "auth_provider_x509_cert_url": os.environ.get('U4'),
+  "client_x509_cert_url": os.environ.get('U5')
 }
 
 # fireauths for above 45 group
 secret_above_45 = {
+  "type": "service_account",
   "project_id": os.environ.get('ABOVE45_PROJECT_ID'),
-  "private_key": os.environ.get('PRIVATE_KEY_ABOVE45'),
-  "client_email": os.environ.get('CLIENT_EMAIL_ABOVE45')
+  "private_key": os.environ.get('PRIVATE_KEY_ABOVE45').replace('\\n', '\n'),
+  "client_email": os.environ.get('CLIENT_EMAIL_ABOVE45'),
+  "client_id": os.environ.get('A1'),
+  "auth_uri": os.environ.get('A2'),
+  "token_uri": os.environ.get('A3'),
+  "auth_provider_x509_cert_url": os.environ.get('A4'),
+  "client_x509_cert_url": os.environ.get('A5')
 }
 
 
